e491f54335dd5cec4cb251d5f4830833588969e1
Shoxcie <dim85858@gmail.com>
2023-10-14 12:09:37 +0300

    lab2 save request as a .html file * Create directories /dataset/good and /dataset/bad * Save requested page as /dataset/page.html. Added it to the .gitignore Issue: It runs into a captcha every run

--- Lab2/src/main.py
+++ Lab2/src/main.py
@@ -1,7 +1,20 @@
+from bs4 import BeautifulSoup
 import requests
+import os
+
+OUT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, "dataset"))
+try:
+	os.makedirs(os.path.join(OUT_DIR, "good"))
+	os.makedirs(os.path.join(OUT_DIR, "bad"))
+except FileExistsError:
+	pass
 
 URL = "https://www.kinopoisk.ru/film/535341/reviews/ord/date/status/good/perpage/10/"
 
 page = requests.get(URL)
+soup = BeautifulSoup(page.text, "html.parser")
 
 print(f"Status = {page.status_code}")
+
+with open(f"{os.path.join(OUT_DIR, 'page.html')}", 'w', encoding="utf-8") as file_html:
+	file_html.write(soup.prettify())
