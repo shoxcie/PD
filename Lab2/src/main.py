f99382c5e0433d28987f6c26e726905f9c2394e2
Shoxcie <dim85858@gmail.com>
2023-10-14 12:18:02 +0300

    lab2 add cookie to the request to avoid captcha * Captcha workaround: every time it runs into a captcha it requires me to open browser manually, open the website, solve the captcha, extract the cookie "spravka" and replace it in the code

--- Lab2/src/main.py
+++ Lab2/src/main.py
@@ -11,7 +11,16 @@ except FileExistsError:
 
 URL = "https://www.kinopoisk.ru/film/535341/reviews/ord/date/status/good/perpage/10/"
 
-page = requests.get(URL)
+COOKIE = {
+	"spravka": (
+		"dD0xNjk3MjcwMTM2O2k9MTg4LjEzOC4xODIuMTY5O0Q9NzJDRUMwRTVFQzExRjk0RDlEMDgzMTFBQ0IxMUExRUNEQzQzNkZEQTI4ODAyNzRFRU"
+		"YwNTA3NDQyRjhGM0Q1MzUzQUE0ODE2Qzc3REQyN0Y3Q0VFOUVFRkQzQUYyMDc0Q0QyRkI5NTlCRThFMUU3QThCQ0U5MzAyOEQ4OEYxOEI4NkQ1"
+		"MTE4MDQwRDY1OTVEM0VENDA0N0I2NEQ1NDcyMkEwOTVBNzZEMzM7dT0xNjk3MjcwMTM2NTI5NzcxMTEyO2g9ZjZhMmUzYmMwYzY3NDI4YTZhOT"
+		"ZlOGZhMzUwMmE2NTA="
+	)
+}
+
+page = requests.get(URL, cookies=COOKIE)
 soup = BeautifulSoup(page.text, "html.parser")
 
 print(f"Status = {page.status_code}")
