bc259e40314fd948dbf71fd6dee24722a226e67f
shoxcie <100866618+shoxcie@users.noreply.github.com>
2023-10-30 19:34:39 +0200

    lab3 part1 done
    * create annotation.csv and fill it with abs-path, rel-path and class name

--- Lab3/src/part1.py
+++ Lab3/src/part1.py
@@ -6,11 +6,12 @@ DATA_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, "d
 with open(os.path.join(DATA_DIR, "annotation.csv"), mode='w', newline='') as csvfile:
 	writer = csv.writer(csvfile, delimiter=';')
 	
-	header = ["Abs_Path", "Rel_Path", "Class_Name"]
+	header = ["Absolute_Path", "Relative_Path", "Class_Name"]
 	writer.writerow(header)
 	
-	data = [
-		["Test_1:1", 12, "Test_1:3"],
-		["Test_2:1", 22, "Test_2:3"]
-	]
-	writer.writerows(data)
+	for class_name in ["good", "bad"]:
+		class_dir = os.path.join(DATA_DIR, class_name)
+		for file_name in os.listdir(class_dir):
+			abs_path = os.path.join(class_dir, file_name)
+			rel_path = os.path.relpath(abs_path, os.path.join(DATA_DIR, os.pardir))
+			writer.writerow([abs_path, rel_path, class_name])
