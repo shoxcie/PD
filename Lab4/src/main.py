f0352350d624160520d34d9f88d270aabe2ee88d
shoxcie <100866618+shoxcie@users.noreply.github.com>
2023-11-08 14:54:23 +0200

    lab4 window styling
    * Dark theme for window and all widgets
    * Adjustable text field that is going to be used for displaying comments
    * Auto hideable scrollbar for the text field

--- Lab4/src/main.py
+++ Lab4/src/main.py
@@ -1,9 +1,47 @@
 import tkinter as tk
+import tkinter.ttk as ttk
+
+
+class AutoScrollbar(ttk.Scrollbar):
+	def set(self, low, high):
+		if float(low) <= 0.0 and float(high) >= 1.0:
+			self.pack_forget()
+		else:
+			self.pack(side=tk.RIGHT, fill="y")
+		ttk.Scrollbar.set(self, low, high)
+
 
 window = tk.Tk()
+window.configure(background="black")
 
-greeting = tk.Label(text="Hello, Tkinter")
-greeting.pack()
+frame_controls = tk.Frame(background="black")
+frame_controls.pack()
+
+ttk.Label(
+	master=frame_controls,
+	text="Hello, Tkinter",
+	foreground="white",
+	background="black"
+).pack(padx=10, pady=20, side=tk.LEFT)
+
+ttk.Button(
+	master=frame_controls,
+	text="Click me"
+).pack(padx=10, pady=20)
+
+text = tk.Text(
+	background="black",
+	foreground="white",
+	insertbackground="white",
+	insertwidth=2,
+	border=3
+)
+text.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+vsb = AutoScrollbar(
+	orient="vertical",
+	command=text.yview
+)
+text.configure(yscrollcommand=vsb.set)
 
 window.mainloop()
-
