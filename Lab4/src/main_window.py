5c903a200cb5a499741253be2fbf2886c26f85d0
shoxcie <100866618+shoxcie@users.noreply.github.com>
2023-11-14 10:16:27 +0200

    lab4 wrapped gui into a function

--- Lab4/src/main_window.py
+++ Lab4/src/main_window.py
@@ -13,50 +13,55 @@ class AutoScrollbar(ttk.Scrollbar):
 		ttk.Scrollbar.set(self, low, high)
 
 
-window = tk.Tk()
-window.configure(background="black")
+def gui():
+	window = tk.Tk()
+	window.configure(background="black")
+	
+	controls_frame = tk.Frame(background="black")
+	controls_frame.pack()
+	
+	ttk.Label(
+		master=controls_frame,
+		text="Hello, Tkinter",
+		foreground="white",
+		background="black"
+	).pack(padx=10, pady=20, side=tk.LEFT)
+	
+	ttk.Button(
+		master=controls_frame,
+		text="Click me"
+	).pack(padx=10, pady=20)
+	
+	text = tk.Text(
+		background="black",
+		foreground="white",
+		insertbackground="white",
+		insertwidth=2,
+		border=3
+	)
+	text.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+	
+	vsb = AutoScrollbar(
+		orient="vertical",
+		command=text.yview
+	)
+	text.configure(yscrollcommand=vsb.set)
+	
+	data_dir = tk.filedialog.askdirectory(initialdir=os.sep)
+	print(data_dir)
+	
+	# filepath = tk.filedialog.askopenfilename(
+	# 	initialdir="C:\\",
+	# 	title="Select file",
+	# 	filetypes=(
+	# 		("jpeg files", "*.jpg"),
+	# 		("all files", "*.*")
+	# 	)
+	# )
+	# print(filepath)
+	
+	window.mainloop()
 
-frame_controls = tk.Frame(background="black")
-frame_controls.pack()
 
-ttk.Label(
-	master=frame_controls,
-	text="Hello, Tkinter",
-	foreground="white",
-	background="black"
-).pack(padx=10, pady=20, side=tk.LEFT)
-
-ttk.Button(
-	master=frame_controls,
-	text="Click me"
-).pack(padx=10, pady=20)
-
-text = tk.Text(
-	background="black",
-	foreground="white",
-	insertbackground="white",
-	insertwidth=2,
-	border=3
-)
-text.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-
-vsb = AutoScrollbar(
-	orient="vertical",
-	command=text.yview
-)
-text.configure(yscrollcommand=vsb.set)
-
-data_dir = tk.filedialog.askdirectory(initialdir=os.sep)
-print(data_dir)
-
-# filepath = tk.filedialog.askopenfilename(
-# 	initialdir="C:\\",
-# 	title="Select file",
-# 	filetypes=(
-# 		("jpeg files", "*.jpg"),
-# 		("all files", "*.*")
-# 	)
-# )
-# print(filepath)
-
-window.mainloop()
+if __name__ == '__main__':
+	gui()
