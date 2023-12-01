54e56733af49db603207ac9365ab113fa0a58382
shoxcie <100866618+shoxcie@users.noreply.github.com>
2023-12-01 18:48:26 +0200

    lab5 module package * Idea of the module: comment parser that extracts russian title, english title, year and comment text from a .txt file (going to use regex) * Created a module package * Added dist files to the .gitignore

---
+++ Lab5/src/main.py
@@ -0,0 +1,3 @@
+import movie_comment_parser as mcp
+
+print(mcp.title_rus("path"))
---
+++ Lab5/src/movie_comment_parser/src/parser.py
@@ -0,0 +1,10 @@
+def title_rus(file: str):
+	return f"rus, {file}"
+
+
+def title_eng():
+	return "eng"
+
+
+def year():
+	return 0
